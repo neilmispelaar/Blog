d8692c21672de71b9830bb49cfdaa572bc8d3b58
Neil Mispelaar <neil.mispelaar@outlook.com>
2022-01-14 16:42:48 -0500

    Adjust hover styles

--- .vitepress/theme/components/ui/BlogListItem.vue
+++ .vitepress/theme/components/ui/BlogListItem.vue
@@ -10,7 +10,8 @@
               h-96
               w-full
               object-cover
-              group-hover:scale-125
+              group-hover:scale-110
+              duration-700
               transform transform-gpu
               transition-all
               ease-in-out
@@ -27,7 +28,8 @@
             bg-zinc-100
             flex flex-col
             justify-between
-            group-hover:bg-zinc-400
+            group-hover:bg-emerald-700
+            duration-500
             transform
             transition-all
             ease-in-out
@@ -38,15 +40,15 @@
             <h2
               class="text-3xl font-normal tracking-wide group-hover:text-white"
             >
-              {{ title }}
+              <span class="sr-only">Article title: </span>{{ title }}
             </h2>
-            <div v-if="false">
-              <p>{{ summary }}</p>
-            </div>
           </div>
           <!-- Anchored botton -->
-          <div class="mt-20 mb-5">
-            <p>{{ date.string }}</p>
+          <div class="mt-20 mb-0">
+            <p class="group-hover:text-white">
+              <span class="sr-only">Article publish date: </span
+              >{{ date.string }}
+            </p>
           </div>
         </div>
       </article>
@@ -54,28 +56,54 @@
   </li>
   <!-- Vertical layout -->
   <li v-else class="flex">
-    <a :href="href" class="flex">
+    <a :href="href" class="flex group">
       <article class="flex flex-col">
         <!-- Image at the top -->
-        <div class="flex-shrink-0">
+        <div class="flex-shrink-0 overflow-hidden">
           <img
-            class="h-64 w-full object-cover"
+            class="
+              h-64
+              w-full
+              object-cover
+              group-hover:scale-110
+              duration-700
+              transform transform-gpu
+              transition-all
+              ease-in-out
+            "
             src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
             alt=""
           />
         </div>
         <!-- Content container at the bottom -->
-        <div class="flex-1 bg-zinc-100 p-6 flex flex-col justify-between">
+        <div
+          class="
+            flex-1
+            bg-zinc-100
+            p-6
+            flex flex-col
+            justify-between
+            group-hover:bg-emerald-700
+            duration-500
+            transform
+            transition-all
+            ease-in-out
+          "
+        >
           <!-- Top -->
           <div class="pt-5">
-            <h2 class="text-3xl font-normal tracking-wide">{{ title }}</h2>
-            <div v-if="false">
-              <p>{{ summary }}</p>
-            </div>
+            <h2
+              class="text-3xl font-normal tracking-wide group-hover:text-white"
+            >
+              <span class="sr-only">Article title: </span>{{ title }}
+            </h2>
           </div>
           <!-- Anchored botton -->
           <div class="mt-20 mb-5">
-            <p>{{ date.string }}</p>
+            <p class="group-hover:text-white">
+              <span class="sr-only">Article publish date: </span
+              >{{ date.string }}
+            </p>
           </div>
         </div>
       </article>
