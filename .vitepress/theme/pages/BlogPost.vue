e40d9c55d2a5a73cf5aa5b4951c45f9ab0259cbd
Neil Mispelaar <neil.mispelaar@outlook.com>
2022-01-16 22:48:42 -0500

    Start work on laying out blog articles

--- .vitepress/theme/pages/BlogPost.vue
+++ .vitepress/theme/pages/BlogPost.vue
@@ -1,12 +1,49 @@
 <template>
   <div>
-    BlogPost
-    <content />
+    <content-container>
+      <article>
+        <header>
+          <!-- Heading -->
+          <div class="mx-auto my-32 bg-red-200">
+            <h1 class="max-w-prose text-8xl text-zinc-700">{{ data.title }}</h1>
+          </div>
+          <!-- Summary -->
+        </header>
+
+        <!-- Content
+        <div class="mx-auto prose prose-lg prose-zinc prose-h2:font-medium">
+          <content />
+        </div>
+        -->
+        <div class="mx-auto article">
+          <content />
+        </div>
+
+        <div class="mx-auto prose prose-lg prose-zinc prose-h2:font-medium">
+          <content />
+        </div>
+      </article>
+    </content-container>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useData } from 'vitepress';
 
-export default defineComponent({});
+// Components
+import ContentContainer from '../components/ui/ContentContainer.vue';
+
+export default defineComponent({
+  components: {
+    ContentContainer,
+  },
+  setup() {
+    const { frontmatter: data } = useData();
+
+    // const { title } = useData();
+
+    return { data };
+  },
+});
 </script>
