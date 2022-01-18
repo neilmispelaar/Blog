1abc3e58b704eb6c6a4ddfc87ce856a8cfd806bf
Neil Mispelaar <neil.mispelaar@outlook.com>
2022-01-17 20:47:22 -0500

    Add code syntax highlighting css

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,8 +1,9 @@
-import "./assets/inter/inter.css";
-import "./styles/index.css";
+import './assets/inter/inter.css';
+import './styles/index.css';
+import './styles/code.css';
 
-import DefaultLayout from "./layouts/Default.vue";
-import NotFound from "./pages/NotFound.vue";
+import DefaultLayout from './layouts/Default.vue';
+import NotFound from './pages/NotFound.vue';
 
 export default {
   // override the Layout with a wrapper component that injects the slots
